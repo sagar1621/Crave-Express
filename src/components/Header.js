bc31d12cf0c4d6b0b6dccee477dfd7db501045b3
sagar1621 <sagarbiradar1621@gmail.com>
2023-04-15 20:05:24 +0530

    changed the cart logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,7 +36,24 @@ const Header = () => {
             <li className="px-2 hover:underline hover:underline-offset-4">Contact</li>
           </Link>
           <Link to="/cart">
-            <li className="px-2 hover:underline hover:underline-offset-4">Cart{cartItems.length}</li>
+            {/* <li className="px-2 hover:underline hover:underline-offset-4">Cart{cartItems.length}</li> */}
+            <div className="relative group">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-8 w-8 text-white group-hover:text-gray-400"
+              viewBox="0 0 20 20"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v2H3V4zm-1 3a2 2 0 1 1 4 0v9a1 1 0 0 0 2 0v-9a2 2 0 0 1-4 0zm13-2h1V4a3 3 0 0 0-6 0v1h6zm-8 15a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
+              />
+            </svg>
+            {cartItems.length > 0 && (
+              <span className="absolute top-0 right-0 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
+                {cartItems.length}
+              </span>
+            )}
+          </div>
           </Link>
           
         </ul>
